dc277b2dbdd732495dda96b953e93ec8f6736494
Adam Jenkins <adam@wisecat.net>
2026-07-18 10:55:06 +0000

    Add Japanese (ja) language pack

---
+++ lang/ja/block_oerclient.php
@@ -0,0 +1,43 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Japanese language strings for block_oerclient.
+ *
+ * @package    block_oerclient
+ * @copyright  2026 Adam Jenkins <adam@wisecat.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['browsemore'] = 'もっと見る...';
+$string['error_exchangeunreachable'] = '現在、Exchangeに接続できません。しばらくしてから再度お試しください。';
+$string['error_notregistered'] = 'このサイトはまだOER Exchangeに登録されていない(または承認されていない)ため利用できません。サイト管理者に「サイト管理 > プラグイン > ローカルプラグイン > OER Client」を確認するよう依頼してください。';
+$string['nocatalogresources'] = 'Exchangeにはまだ公開されているリソースがありません。';
+$string['nosharesyet'] = 'このサイトからExchangeへ共有されたものはまだありません。';
+$string['oerclient:addinstance'] = '新しいOER Exchangeブロックを追加する';
+$string['oerclient:myaddinstance'] = 'ダッシュボードに新しいOER Exchangeブロックを追加する';
+$string['pluginname'] = 'OER Exchange';
+$string['privacy:metadata'] = 'OER Exchangeブロックは、local_oerclientによってすでに保存されている共有情報と、接続されたExchangeの公開リソースを表示するだけであり、独自のデータは保存しません。';
+$string['recentpaneltitle'] = '利用可能な最新のOER';
+$string['sharespaneltitle'] = '自分が共有したもの';
+$string['status_backingup'] = 'バックアップを作成中';
+$string['status_failed'] = '失敗';
+$string['status_pending'] = '待機中';
+$string['status_published'] = '公開済み';
+$string['status_uploading'] = 'アップロード中';
+$string['unknowncourse'] = '不明なコース';
